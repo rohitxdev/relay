18c5914c355b9beca8550f96e4b2899faeda4a75
rohitman47 <rohitreddy.gangwar@gmail.com>
2022-07-09 17:29:36 +0530

    Fix heroku error

--- server/src/index.ts
+++ server/src/index.ts
@@ -98,6 +98,6 @@ server.get("/api/get-og-image", async (request, reply) => {
   reply.type("image/jpeg").send(stream);
 });
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server is listening to http://${HOST}:${PORT}`);
+server.listen(PORT, () => {
+  console.log(`Server is running.`);
 });
