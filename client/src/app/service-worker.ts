d97578294d82f3eeb22577e7aeadeb2fb4094760
rohitman47 <rohitreddy.gangwar@gmail.com>
2022-09-15 17:19:54 +0530

    Change manifest orientation

--- client/src/app/service-worker.ts
+++ client/src/app/service-worker.ts
@@ -1,7 +1,7 @@
 declare const self: ServiceWorkerGlobalScope;
 
 export const serviceWorker = () => {
-  const cacheVersion = "v1.0.11";
+  const cacheVersion = "v1.0.12";
   const cacheName = `relay-cache-${cacheVersion}`;
   const indexURL = new URL("/", import.meta.url);
 
